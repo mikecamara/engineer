c2fae8d7833afaf0ecca63f047a1792b7d5bbc0d
Mike Camara <mcmikecamara@gmail.com>
2022-08-02 00:14:56 +1000

    Add script

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Script from 'next/script'
 export default function Header({ name }) {
   return (
     <>
-      <iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0Vl_8qUuNG8c7giECClFDati0NUPsJFXfiFe3VYFnU6pg68wdBA0chrWMqSjLYOlk9nn3rrcgA?gv=true" style="border: 0" width="100%" height="600" frameborder="0"></iframe>
+      <iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ0Vl_8qUuNG8c7giECClFDati0NUPsJFXfiFe3VYFnU6pg68wdBA0chrWMqSjLYOlk9nn3rrcgA?gv=true" style="border: 0" width="100%" height="600" frameBorder="0"></iframe>
     </>
   );
 }
